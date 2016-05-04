2037a5277073894e8205dbd2a1c5b1fc0ffa259b
willcwelch <willcwelch@gmail.com>
2016-05-04 04:43:46 -0400

    Changed default layout loading.

--- src/Template/Layout/default.ctp
+++ src/Template/Layout/default.ctp
@@ -26,8 +26,8 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     </title>
     <?= $this->Html->meta('icon') ?>
 
-    <?= $this->Html->css('base.css') ?>
-    <?= $this->Html->css('cake.css') ?>
+    <?= $this->Html->css('bootstrap/bootstrap.css'); ?>
+    <?= $this->Html->script(['jquery/jquery.js', 'bootstrap/bootstrap.js']); ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
--- src/View/AppView.php
+++ src/View/AppView.php
@@ -13,7 +13,8 @@
  */
 namespace App\View;
 
-use BootstrapUI\View\UIView;
+use Cake\View\View;
+use BootstrapUI\View\UIViewTrait;
 
 /**
  * Application View
@@ -22,8 +23,10 @@ use BootstrapUI\View\UIView;
  *
  * @link http://book.cakephp.org/3.0/en/views.html#the-app-view
  */
-class AppView extends UIView
+class AppView extends View
 {
+
+    use UIViewTrait;
 
     /**
      * Initialization hook method.
@@ -36,6 +39,8 @@ class AppView extends UIView
      */
     public function initialize()
     {
-        parent::initialize();
+        //render the initializeUI method from the UIViewTrait
+        $this->initializeUI(['layout' => false]);
+
     }
 }
